b4000e2cbb9c886c639aad469d0d65accdbbb426
dichev <g.dichev@gmail.com>
2020-05-16 11:27:30 +0200

    more utils

--- lib/calc.py
+++ lib/calc.py
@@ -1,8 +1,23 @@
 import numpy as np
+from collections import Counter
 
 def mean_cum(count, mean, amount):
     sum = mean * count
     return (sum + amount) / (count + 1)
+
+
+def sum_collection(lst:list) -> dict:
+    counter = Counter()
+    for obj in lst:
+        counter.update(obj)
+    return dict(counter)
+
+def mean_collection(lst:list) -> dict:
+    n = len(lst)
+    sums = sum_collection(lst)
+    means = { key: val/n for key, val in sums.items()}
+    return means
+
 
 def softmax(values, tau = 1):
     exp = np.exp(np.array(values) / tau)
---
+++ lib/utils.py
@@ -0,0 +1,22 @@
+
+def chunks(lst:list, n:int):
+    """Yield successive n-sized chunks from lst."""
+    for i in range(0, len(lst), n):
+        yield lst[i:i + n]
+
+
+class AttrDict(dict):
+    """ Dictionary subclass whose entries can be accessed by attributes
+        (as well as normally).
+    """
+    def __init__(self, *args, **kwargs):
+        super(AttrDict, self).__init__(*args, **kwargs)
+        self.__dict__ = self
+
+
+def dict_as_object(data:dict) -> AttrDict:
+    """ Construct nested AttrDicts from nested dictionaries. """
+    if not isinstance(data, dict):
+        return data
+    else:
+        return AttrDict({key: dict_as_object(data[key]) for key in data})
